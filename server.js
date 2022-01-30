72d4d73c12acc623cbca53a40a301092c1695bdf
Evan Cheung <evan.d.cheung@gmail.com>
2022-01-30 19:12:36 +0000

    Added boilerplate for server and client html, as well as added packages in replit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+
+var io = new Server(server);
+
+
+io.on('connection', function(socket) {
+  //run console log on connect
+  console.log("Player connected at ID " + socket.id);
+
+  //add event handler to disconnect
+  socket.on('disconnect', function() {
+    console.log("Player disconnected at ID " + socket.id)
+  });
+});
+
+const port = 8081;
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(port, function() {
+  console.log(`Listening on ${server.address().port}`);
+});
