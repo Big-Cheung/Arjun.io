f6ecac5570d17926bc89318ea32a0a7a56f4826c
ishaanmody1 <ishaanmody1@gmail.com>
2022-02-28 15:51:15 -0800

    improved UI of login screen
    working on getting Material Design working with imports (unusual Babel error); for now this works

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,6 @@
-// import "./styles.css";
 
+// const [errorMessages, setErrorMessages] = React.useState({});
+//   const [isSubmitted, setIsSubmitted] = React.useState(false);
 function App() {
   // React States
   const [errorMessages, setErrorMessages] = React.useState({});
@@ -53,8 +54,10 @@ function App() {
 
   // JSX code for login form
   const renderForm = (
+    
     <div className="form">
-      <form onSubmit={handleSubmit}>
+    <script src="../styles.css" type="text/babel"></script>
+      <form onSubmit={handleSubmit} class="form" id="login">
         <div className="input-container">
           <label>Username </label>
           <input type="text" name="uname" required />
@@ -82,5 +85,86 @@ function App() {
   );
 }
 
+
+const handleSubmit = (event) => {
+  //Prevent page reload
+  event.preventDefault();
+
+  var { uname, pass } = document.forms[0];
+
+  // Find user login info
+  
+};
+const mystyle = {
+  color: "white",
+  backgroundColor: "#ffa64d",
+  padding: "15px",
+  fontFamily: "Arial",
+  innerHeight: "300px",
+  innerWidth: "200px",
+  outerWidth: "200px",
+};
+
+const cardStyles = {
+  container: {
+    display: "flex",
+    height: 100,
+    width: 400,
+    boxShadow: "0 0 3px 2px #cec7c759",
+    alignItems: "center",
+    padding: 20,
+    borderRadius: 20,
+  },
+  field: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "gray",
+    color: "white",
+    height: 40,
+    width: 200,
+    padding: 10,
+    fontWeight: "bold",
+  },
+  button: {
+    marginLeft: 10,
+    backgroundColor: "blue",
+    color: "white",
+    height: 20,
+    width: 60,
+    fontWeight: "bold",
+  },
+  userName: {
+    fontWeight: "bold",
+  },
+};
+
+const renderForm = (
+  <div className="form">
+  <script src="../styles.css" type="text/babel"></script>
+    <form onSubmit={handleSubmit} style={cardStyles.container}>
+      <div className="input-container" style={cardStyles.field} >
+        <label>Username </label>
+        <input type="text" name="uname" />
+        
+      </div>
+      <div className="input-container" style={cardStyles.field}>
+        <label>Password </label>
+        <input type="password" name="pass" />
+        
+      </div>
+      <div className="btn btn-primary">
+        <input type="submit" style={cardStyles.button}/>
+      </div>
+    </form>
+
+  </div>
+);
+
+class Login extends React.Component{
+  render(){
+    return renderForm
+  }
+}
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Login />, rootElement);
