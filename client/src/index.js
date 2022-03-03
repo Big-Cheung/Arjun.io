db078403e2a15ae715e8b3efa9ff441cf1223e4d
Evan Cheung <evan.d.cheung@gmail.com>
2022-03-03 14:44:41 -0800

    Removed web vitals!

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles.css';
-import reportWebVitals from './reportWebVitals';
 import game from "./game.js"
 
 
@@ -15,11 +14,3 @@ ReactDOM.render(
 );
 game();
 
-
-
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
