7eab7c211aefa18e1f1e202fa7598ab4ff303976
Joon <joonwshin@gmail.com>
2022-03-05 18:33:38 -0800

    Added expanded leaderboard and pagination

--- client/src/Leaderboard.js
+++ client/src/Leaderboard.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { RGBA_ASTC_10x10_Format } from 'three';
+import React from 'react';
+import Pagination from './Pagination';
 
 // testdata to be removed later
 const testdata = [
@@ -8,25 +8,57 @@ const testdata = [
   {username: 'name3', score: 30},
   {username: 'name4', score: 40},
   {username: 'name5', score: 50},
+  {username: 'name6', score: 60},
+  {username: 'name7', score: 70},
+  {username: 'name8', score: 80},
+  {username: 'name9', score: 90},
+  {username: 'name10', score: 100},
+  {username: 'name11', score: 110},
+  {username: 'name12', score: 120},
+  {username: 'name13', score: 130},
+  {username: 'name14', score: 140},
+  {username: 'name15', score: 150},
+  {username: 'name16', score: 160},
+  {username: 'name17', score: 170},
+  {username: 'name18', score: 180},
+  {username: 'name19', score: 190},
+  {username: 'name20', score: 200},
 ];
 
-// Minimized Leaderboard
 export default function Leaderboard() {
+  
+  // Leaderboard Button
+  const [open, setOpen] = React.useState(false);
+
+  // Process Data
   let sorted = testdata.sort((a, b) => b.score - a.score);
   let userlist = sorted.map((user, i) =>
   <User username={user.username} rank={i + 1} score={user.score} />
   );
-  return (
-    <div className="minimized-container" style = {minimizedStyle.container}>
-      <LeaderboardButton/>      
-      {userlist.slice(0, 5)}
-    </div>
-  )
-};
+  
+  // Pagination
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const [usersPerPage] = React.useState(10);
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const currentUsers = userlist.slice(indexOfFirstUser, indexOfLastUser);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-const LeaderboardButton = () => {
   return (
-    <button className = "leaderboard-button" onClick={handleClick} style={minimizedStyle.header}>Leaderboard</button>
+    <React.Fragment>
+      <div className="minimized-container" style={minimizedStyle.container}>
+        <button className = "leaderboard-button" onClick={() => setOpen(!open)} data-toggle = "modal" style={minimizedStyle.header}>Leaderboard</button>     
+        {userlist.slice(0, 5)}
+      </div>
+      {open && (
+        <div className="expanded-container" style={expandedStyle.container}>
+          <div className="expanded-header" style={expandedStyle.header}>Leaderboard</div>
+          <ColumnHeader/>
+          {currentUsers}
+          <Pagination usersPerPage={usersPerPage} totalUsers={userlist.length} paginate={paginate}/>
+        </div>
+      )}
+    </React.Fragment>   
   )
 };
 
@@ -46,43 +78,92 @@ const User = ({ rank, username, score}) => {
   )
 };
 
-const handleClick = () => {
-    console.log("works");
+const ColumnHeader = () => {
+  return(
+    <div className="col-header" style={expandedStyle.colheader}>
+      <div className="col-rank" style={expandedStyle.colrank}>
+        <div className="expanded-text" style={expandedStyle.coltext}>#</div>
+      </div>
+      <div className="col-name" style={expandedStyle.colname}>
+        <div className="expanded-text" style={expandedStyle.coltext}>Name</div>
+      </div>
+      <div className="col-score" style={expandedStyle.colscore}>
+        <div className="expanded-text" style={expandedStyle.coltext}>Score</div>
+      </div>
+  </div>
+  ) 
 }
 const minimizedStyle = {
-    container: {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      padding: 5,
-      borderRadius: 5,
-      float: "right",
-      backgroundColor: "rgba(0, 0, 0, 0.2)",
-    },
-    header: {
-      fontWeight: "bold",
-      fontSize: 15,
-      backgroundColor: "transparent",
-      textAlign: "center",
-    },
-    text: {
-      fontSize: 11,
-    },
-    user: {
-      display: "flex",
-      flexDirection: "row",
-      flexWrap: "nowrap",
-      alignItems: "center",
-      textAlign: "center",
-      padding: 5,
-    },
-    rank: {
-      paddingLeft: 5,
-    },
-    username: {
-      paddingLeft: 5,
-    },
-    score: {
-      paddingLeft: 5,
-    }
-  };
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: 5,
+    borderRadius: 5,
+    float: "right",
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+  },
+  header: {
+    fontWeight: "bold",
+    fontSize: 15,
+    backgroundColor: "transparent",
+    textAlign: "center",
+  },
+  text: {
+    fontSize: 11,
+  },
+  user: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    alignItems: "center",
+    textAlign: "center",
+    padding: 5,
+  },
+  rank: {
+    paddingLeft: 5,
+  },
+  username: {
+    paddingLeft: 5,
+  },
+  score: {
+    paddingLeft: 5,
+  },
+};
+
+const expandedStyle = {
+  container: {
+    backgroundColor: "white",
+    height: 400,
+    width: 400,
+    margin: "auto",
+    top: 100,
+  },
+  header: {
+    fontWeight: "bold",
+    fontSize: 30,
+    textAlign: "center",
+  },
+  text: {
+    fontSize: 11,
+  },
+  colheader: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  colrank: {
+    paddingLeft: 5,
+  },
+  colname: {
+    paddingLeft: 5,
+  },
+  colscore: {
+    paddingLeft: 5,
+  },
+  coltext: {
+    fontSize: 20,
+  }
+}
