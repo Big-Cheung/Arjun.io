d4aff4697246967ce9417b3a97c02db79a6e82e0
Evan Cheung <evan.d.cheung@gmail.com>
2022-03-11 00:34:58 -0800

    timer is working now lol
    great code

--- client/src/timer.js
+++ client/src/timer.js
@@ -4,65 +4,49 @@ import Button from '@material-ui/core/Button';
 import {listen} from './events.js'
 import { StylesContext } from '@material-ui/styles';
 
+function getTimeLeft(time){
+  const currTime = Date.now();
+  const total = time - currTime
+  const seconds = Math.floor((total/1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  return [total, minutes, seconds];
+}
+
+function getTimerText(timeLeft) {
+  let [total, minutes, seconds] = timeLeft;
+  return (minutes > 9 ? minutes : '0' + minutes) + ':' + (seconds > 9 ? seconds : '0' + seconds);
+}
+
+function timerLoop(endTime,func,stopfunc) {
+  let times = getTimeLeft(endTime);
+  console.log(endTime);
+  if (times[0] > 0) {
+    func(getTimerText(times));
+    setTimeout(() => (timerLoop(endTime,func,stopfunc)),times[0]%1000);
+  } else {
+    stopfunc(false)
+  }
+}
+
 const GameTimer = () => {
     const [GameTimer, setGameTimer] = React.useState(false);
     const [timer, setTimer] = React.useState('00:00');
     const [end, setEnd] = React.useState();
-    const Ref = React.useRef(null);
+
     React.useEffect(()=>{
       listen("endTime", (args) => {
-        console.log("time = ",args[0]);
+        console.log("got time as ",args[0]);
         setEnd(args[0]);
-        console.log("game state = ",args[1]);
-        setGameTimer(args[1]);
-      })},[])
-    function getTimeLeft(time){
-        const currTime = Date.now();
-        const total = time - currTime
-        const seconds = Math.floor((total/1000) % 60);
-        const minutes = Math.floor((total / 1000 / 60) % 60);
-        return {
-            total, minutes, seconds
-        };
-    }
-    function startTimer(time) {
-        let {total, minutes, seconds} = getTimeLeft(time);
-        setTimer(
-            (minutes > 9 ? minutes : '0' + minutes) + ':'
-            + (seconds > 9 ? seconds : '0' + seconds)
-        )
-        console.log(timer)
-    }
-    if (Ref.current) 
-    clearInterval(Ref.current);
-    const id = setInterval(() => {
-    startTimer(end);
-    }, 1000)
-    Ref.current = id;
-    
-    if (GameTimer === 1)
-    {
-    if (timer === '00:00')
-    {
-      return null
-    }
-    else{
+        setGameTimer(true);
+        setTimer(getTimerText(getTimeLeft(args[0])))
+        timerLoop(args[0],setTimer,setGameTimer);
+    })},[])
+
     return (
       <div>
-        <h1 style = {cardStyles.timer}> {timer}</h1>
+        <h1 style = {cardStyles.timer}>{GameTimer ? timer : ""}</h1>
       </div>
     );
-    }
-  }
-  
-  else
-  {
-    return (
-        <div>
-        </div>
-      );
-      
-  }
 }
 const cardStyles = {
     container: {
