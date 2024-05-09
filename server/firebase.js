1ae9514d7afff67165519696869760ad0822fb17
Evan Cheung <evan.d.cheung@gmail.com>
2024-05-09 10:46:49 -0700

    Update firebase.js

--- server/firebase.js
+++ server/firebase.js
@@ -13,13 +13,13 @@ const {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB1U7iVCc3E--AwAcDv6td2AZaM8YyOxNY",
-  authDomain: "arjunio.firebaseapp.com",
-  databaseURL: "https://arjunio-default-rtdb.firebaseio.com",
-  projectId: "arjunio",
-  storageBucket: "arjunio.appspot.com",
-  messagingSenderId: "1024233461521",
-  appId: "1:1024233461521:web:827df22c397ef47e7a9eb0"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "=",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
